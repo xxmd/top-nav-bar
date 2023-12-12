64268dbd389161b505222255b912553e795d781c
GMJ <410515742@qq.com>
2023-12-12 15:24:58 +0800

    add custom attribute

--- mylibrary/src/main/java/io/github/xxmd/TopNavBar.java
+++ mylibrary/src/main/java/io/github/xxmd/TopNavBar.java
@@ -3,6 +3,7 @@ package io.github.xxmd;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
@@ -87,29 +88,60 @@ public class TopNavBar extends FrameLayout {
     }
 
     private void initViewByTypedArray() {
-        Drawable leftIcon = typedArray.getDrawable(R.styleable.TopNavBar_leftIcon);
-        if (leftIcon != null) {
-            binding.ivLeftIcon.setImageDrawable(leftIcon);
+        int tintColor = typedArray.getColor(R.styleable.TopNavBar_tint, Integer.MIN_VALUE);
+        if (tintColor != Integer.MIN_VALUE) {
+            binding.ivLeftIcon.setColorFilter(tintColor);
+            binding.tvTitle.setTextColor(tintColor);
+            binding.tvSubtitle.setTextColor(tintColor);
+            binding.ivRightIcon.setColorFilter(tintColor);
+            binding.tvRightText.setTextColor(tintColor);
         }
-        setText(R.styleable.TopNavBar_title, binding.tvTitle);
-        setText(R.styleable.TopNavBar_subtitle, binding.tvSubtitle);
-        setText(R.styleable.TopNavBar_rightText, binding.tvRightText);
 
-        Drawable rightIcon = typedArray.getDrawable(R.styleable.TopNavBar_rightIcon);
-        if (rightIcon != null) {
-            binding.ivRightIcon.setImageDrawable(rightIcon);
-        } else {
-            binding.ivRightIcon.setVisibility(GONE);
-        }
+
+        setIcon(R.styleable.TopNavBar_leftIcon, R.styleable.TopNavBar_leftIconTint, binding.ivLeftIcon, false);
+
+        setText(R.styleable.TopNavBar_title, R.styleable.TopNavBar_titleColor, binding.tvTitle);
+        setText(R.styleable.TopNavBar_subtitle, R.styleable.TopNavBar_subtitleColor, binding.tvSubtitle);
+        setText(R.styleable.TopNavBar_rightText, R.styleable.TopNavBar_rightTextColor, binding.tvRightText);
+
+        setIcon(R.styleable.TopNavBar_rightIcon, R.styleable.TopNavBar_rightIconTint, binding.ivRightIcon, true);
     }
 
-    private void setText(@StyleableRes int index, TextView textView) {
+    private void setIcon(@StyleableRes int index, @StyleableRes int colorIndex, ImageView imageView, boolean autoHidden) {
+        Drawable icon = typedArray.getDrawable(index);
+        if (icon != null) {
+            imageView.setImageDrawable(icon);
+        } else {
+            if (autoHidden) {
+                imageView.setVisibility(GONE);
+            }
+        }
+        setColorFilter(colorIndex, imageView);
+    }
+
+    private void setText(@StyleableRes int index, @StyleableRes int colorIndex, TextView textView) {
         String text = typedArray.getString(index);
         if (StringUtils.isEmpty(text)) {
             textView.setVisibility(GONE);
         } else {
             textView.setVisibility(VISIBLE);
             textView.setText(text);
+        }
+
+        setColor(colorIndex, textView);
+    }
+
+    private void setColor(@StyleableRes int index, TextView textView) {
+        int color = typedArray.getColor(index, Integer.MIN_VALUE);
+        if (color != Integer.MIN_VALUE) {
+            textView.setTextColor(color);
+        }
+    }
+
+    private void setColorFilter(@StyleableRes int index, ImageView imageView) {
+        int color = typedArray.getColor(index, Integer.MIN_VALUE);
+        if (color != Integer.MIN_VALUE) {
+            imageView.setColorFilter(color);
         }
     }
 }
